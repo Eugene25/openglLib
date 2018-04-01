21f1f3385d02170d41d99a02d20b7e6c5c4d7966
Eugene25 <21500766@handong.edu>
2018-04-01 22:10:19 +0900

    Update building.cpp

--- building.cpp
+++ building.cpp
@@ -9,6 +9,7 @@
 #include <iostream>
 
 #include "createCity.h"
+#include "popUp.h"
 
 #include <math.h>
 #ifdef __APPLE__
@@ -21,11 +22,9 @@
 using namespace std;
 
 /*create brick texture*/
-int textureNum = 7;
-GLuint texture[7];
 
 /*move car*/
-float theta = 0.0, angle = 0.0, posX = 0, posY = 0, posZ = 0;
+float angle = 0.0, posX = 0, posY = 0, posZ = 0;
 
 GLBatch				topBlock;
 GLBatch				frontBlock;
@@ -35,37 +34,37 @@ GLMatrixStack		projectionMatrix;
 
 int w, h;
 
-float move_unit = 0.1f;
-
 createCity::createCity(){}
 createCity::~createCity(){}
+
+static float move_unit = 0.1f;
+
+void display(void);
 
 
 void keyboardown(int key, int x, int y)
 {
+	cout << "speed = " << move_unit << "\n";
+
 	switch (key){
 	case GLUT_KEY_RIGHT:
 		angle = 180;
-		posX += move_unit;;
-		cout << x << endl;
+		posX += move_unit;
 		break;
 
 	case GLUT_KEY_LEFT:
 		angle = 0;
-		posX -= move_unit;;
-		cout << x << endl;
+		posX -= move_unit;
 		break;
 
 	case GLUT_KEY_UP:
 		angle = -90;
-		posY += move_unit;;
-		cout << y << endl;
+		posY += move_unit;
 		break;
 
 	case GLUT_KEY_DOWN:
 		angle = 90;
-		posY -= move_unit;;
-		cout << y << endl;
+		posY -= move_unit;
 		break;
 
 	default:
@@ -74,201 +73,33 @@ void keyboardown(int key, int x, int y)
 	glutPostRedisplay();
 }
 
-void setTexture(void) {
-
-	GLbyte *pBytes;
-	GLint nWidth, nHeight, nComponents;
-	GLenum format;
-
-	glClearColor(1.0, 1.0, 1.0, 0.0);
-	glShadeModel(GL_FLAT);
-	glEnable(GL_DEPTH_TEST);
-	glPixelStorei(GL_UNPACK_ALIGNMENT, 2);
-
-	glGenTextures(textureNum, texture);
-
-
-	pBytes = gltReadTGABits("brick.tga", &nWidth, &nHeight, &nComponents, &format);
-	glBindTexture(GL_TEXTURE_2D, texture[0]);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	glTexImage2D(GL_TEXTURE_2D, 0, nComponents, nWidth, nHeight, 0,
-		format, GL_UNSIGNED_BYTE, pBytes);
-	free(pBytes);
-
-	pBytes = gltReadTGABits("window.tga", &nWidth, &nHeight, &nComponents, &format);
-	glBindTexture(GL_TEXTURE_2D, texture[1]);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	glTexImage2D(GL_TEXTURE_2D, 0, nComponents, nWidth, nHeight, 0,
-		format, GL_UNSIGNED_BYTE, pBytes);
-	free(pBytes);
-
-	pBytes = gltReadTGABits("window2.tga", &nWidth, &nHeight, &nComponents, &format);
-	glBindTexture(GL_TEXTURE_2D, texture[2]);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	glTexImage2D(GL_TEXTURE_2D, 0, nComponents, nWidth, nHeight, 0,
-		format, GL_UNSIGNED_BYTE, pBytes);
-	free(pBytes);
-
-	pBytes = gltReadTGABits("map.tga", &nWidth, &nHeight, &nComponents, &format);
-	glBindTexture(GL_TEXTURE_2D, texture[3]);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	glTexImage2D(GL_TEXTURE_2D, 0, nComponents, nWidth, nHeight, 0,
-		format, GL_UNSIGNED_BYTE, pBytes);
-	free(pBytes);
-
-	pBytes = gltReadTGABits("logo.tga", &nWidth, &nHeight, &nComponents, &format);
-	glBindTexture(GL_TEXTURE_2D, texture[4]);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	glTexImage2D(GL_TEXTURE_2D, 0, nComponents, nWidth, nHeight, 0,
-		format, GL_UNSIGNED_BYTE, pBytes);
-	free(pBytes);
-
-	pBytes = gltReadTGABits("school.tga", &nWidth, &nHeight, &nComponents, &format);
-	glBindTexture(GL_TEXTURE_2D, texture[5]);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	glTexImage2D(GL_TEXTURE_2D, 0, nComponents, nWidth, nHeight, 0,
-		format, GL_UNSIGNED_BYTE, pBytes);
-	free(pBytes);
-
-	pBytes = gltReadTGABits("car.tga", &nWidth, &nHeight, &nComponents, &format);
-	glBindTexture(GL_TEXTURE_2D, texture[6]);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	glTexImage2D(GL_TEXTURE_2D, 0, nComponents, nWidth, nHeight, 0,
-		format, GL_UNSIGNED_BYTE, pBytes);
-	free(pBytes);
-}
-
 void init(void) {
 
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	//gluOrtho2D(0,10, 0,10);
-	glMatrixMode(GL_MODELVIEW);
-
-
-
-}
-
-
-void initOrtho(int x, int y, int width, int height) {
 	//glMatrixMode(GL_PROJECTION);
 	//glLoadIdentity();
-	gluOrtho2D(x, y, width, height);
+	//gluOrtho2D(0,10, 0,10);
 	//glMatrixMode(GL_MODELVIEW);
-}
 
-void drawCar() {
 
-	glBindTexture(GL_TEXTURE_2D, texture[6]);
-	glTranslatef(posX, posY, 0.0);
-	glRotatef(angle, 0.0, 0.0, 1.0);
-	glTranslatef(-posX, -posY, 0.0);
-	glTranslatef(posX, posY, 0.0);
-
-	std::cout << "posX = " << posX << ", posY = " << posY << "\n";
-	gluOrtho2D(-10.0, 10.0, -10.0, 10.0);
-	glBegin(GL_POLYGON);
-	glTexCoord2f(0, 0);
-	glVertex2f(-0.5, -0.5);
-	glTexCoord2f(0, 1.0f);
-	glVertex2f(0.5, -0.5);
-	glTexCoord2f(1.0f, 1.0f);
-	glVertex2f(0.5, 0.5);
-	glTexCoord2f(1.0f, 0);
-	glVertex2f(-0.5, 0.5);
-	glEnd();
 
 }
 
-void drawMapAndCity() {
+void menuSelect() {
+	if (value == 1){ //restart
+		angle = 0.0, posX = 0, posY = 0, posZ = 0;
+		glutDisplayFunc(display);
+		value = 0;
 
-	/* first apart building */
-
-	gluOrtho2D(-15, 35, -20, 35);
-	createWindow(texture[1]);
-	createBuilidng(texture[0], 0, 0, 0, 0, 0);
-
-	/* second apple office */
-
-	initOrtho(-23.5, 35, -50, 5);
-	createCircle(texture[4]);
-	initOrtho(-15, 35, -50, 10);
-	createBuilidng(texture[2], 0, 0, 0, 0, 0);
-
-	/* third building with star */
-
-	glColor3f(1, 1, 0);
-	initOrtho(-100, 27, -120, 65);
-	createStar();
-
-
-	initOrtho(-50, 20, -30, 20);
-	createWindow(texture[1]);
-	createBuilidng(texture[0], 2, 0, 1.8, 2, 0);
-
-
-	/* fourth school */
-	initOrtho(2, 50, -20, 35);
-	createWindow(texture[1]);
-	initOrtho(-2, 50, -20, 35);
-	createWindow(texture[1]);
-
-	initOrtho(-2, 50, -28, 35);
-	createFlag();
-
-	initOrtho(0, 50, -20, 35);
-	createBuilidng(texture[5], 3, 0, 3, 3, 0);
-
-
-	/* fifth restroom */
-	initOrtho(0, 60, -50, 10);
-	createRoof();
-	createWindow(texture[1]);
-	createBuilidng(texture[0], 2, 0, 1, 2, 0);
-
-
-	/*background map*/
-
-	initOrtho(0, 10, 0, 10);
-
-	glBindTexture(GL_TEXTURE_2D, texture[3]);
-
-	glBegin(GL_QUADS);
-	glTexCoord2f(0, 0);
-	glVertex3f(0, 0, 0); //Left bottom
-
-	glTexCoord2f(1.0f, 0);
-	glVertex3f(10, 0, 0); //Right bottom
-
-	glTexCoord2f(1.0f, 1.0f);
-	glVertex3f(10, 10, 0); //Right top
-
-	glTexCoord2f(0, 1.0f);
-	glVertex3f(0, 10, 0); //Left top
-	glEnd();
-
-	
+	}
+	else if (value == 2){ //slow
+		move_unit = 0.03f;
+	}
+	else if (value == 3){ //medium
+		move_unit = 0.1f;
+	}
+	else if (value == 4){ //fast
+		move_unit = 0.5f;
+	}
 }
 
 
@@ -280,19 +111,22 @@ void display(void) {
 	glEnable(GL_TEXTURE_2D);
 	glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
 
+	menuSelect();
+
 	glViewport(0, (1.5*h) / 5, w, (3.5*h) / 5);
+
 	glPushMatrix();
-
-	drawCar();
-	drawMapAndCity();
-	
-
+	drawCar(posX,posY,angle);
 	glPopMatrix();
+
+	glPushMatrix();
+	drawMapAndCity();
+	glPopMatrix();
+
 	glDisable(GL_TEXTURE_2D);
 	glutSwapBuffers();
 
 }
-
 
 
 int main(int argc, char **argv){
@@ -315,8 +149,9 @@ int main(int argc, char **argv){
 	}
 	setTexture();
 	init();
-	glutDisplayFunc(display);
 
+	 createMenuCG();
+	glutDisplayFunc(display);
 	glutSpecialFunc(keyboardown);
 
 
