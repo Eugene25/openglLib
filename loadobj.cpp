47b16b4f50566b987546c67d7b08c43c1c3d2c37
Eugene25 <21500766@handong.edu>
2018-05-18 12:29:04 +0900

    Update loadobj.cpp

--- loadobj.cpp
+++ loadobj.cpp
@@ -41,132 +41,140 @@ char ch = '1';
 FILE *fp;
 GLfloat indicies[];
 
-float vertices[1000];
-float faces[1000];
+//float vertices[1000];
+//float faces[1000];
 
 using namespace std;
 using namespace glm;
+
+unsigned int VAO;
+unsigned int VBO;
+unsigned int VBO_verts;
+unsigned int  VBO_indice;
+unsigned int  VBO_normals;
+
+std::vector< glm::vec3 > vertices;
+std::vector< glm::vec2 > uvs;
+std::vector< glm::vec3 > normals;
+
+bool loadOBJ(
+	const char * path,
+	std::vector < glm::vec3 > & out_vertices,
+	std::vector < glm::vec2 > & out_uvs,
+	std::vector < glm::vec3 > & out_normals
+	);
 
 // ----------------------------------------------------------
 // display() Callback function
 // ----------------------------------------------------------
 
 
-void loadObj(char *fname) {
+bool loadOBJ(
+const char * fname,
+std::vector < glm::vec3 > & out_vertices,
+std::vector < glm::vec2 > & out_uvs,
+std::vector < glm::vec3 > & out_normals
+) {
 
+	std::vector< unsigned int > vertexIndices, uvIndices, normalIndices;
+	std::vector< glm::vec3 > temp_vertices;
+	std::vector< glm::vec2 > temp_uvs;
+	std::vector< glm::vec3 > temp_normals;
 
-	int read;
 	GLfloat x, y, z;
-	char ch;
-	int i = 0;
-	int j = 0;
 
-
-	big = glGenLists(1);
-	fp = fopen(fname, "r");
-	if (!fp) {
-		printf("can't open file %s \n", fname);
-		exit(1);
+	FILE * file = fopen(fname, "r");
+	if (file == NULL){
+		printf("Impossible to open the file !\n");
 	}
-	glPointSize(2.0);
-	glNewList(big, GL_COMPILE);
 
-	{
-		glPushMatrix();
-		glBegin(GL_POINTS);
-		while (!(feof(fp))) {
-			read = fscanf(fp, "%c %f %f %f", &ch, &x, &y, &z);
-			if (read == 4 && ch == 'v') {
-				//std::cout << "(x,y,z) = (" << x << ", " << y << ", " << z << ") \n";
-				glVertex3f(x, y, z);
-				vertices[i] = x;
-				vertices[i+1] = y;
-				vertices[i+2] = z;
-				//std::cout << "(vertices : x,y,z) = (" << x << ", " << y << ", " << z << ") \n";
+	while (1){
 
-				i += 3;
-				//printf("current index [i] = %d \n", i);
-			}
-			if (read == 4 && ch == 'f') {
-				//std::cout << "(x,y,z) = (" << x << ", " << y << ", " << z << ") \n";
-				glVertex3f(x, y, z);
-				faces[j] = x;
-				faces[j + 1] = y;
-				faces[j + 2] = z;
-				//std::cout << "faces" << j << ": (x,y,z) = (" << x << ", " << y << ", " << z << ") \n";
+		char lineHeader[128];
+		// read the first word of the line
+		int res = fscanf(file, "%s", lineHeader);
+		if (res == EOF)
+			break; // EOF = End Of File. Quit the loop.
 
-				j += 3;
-				//printf("current index = %d \n", j);
-			}
-			
+		if (strcmp(lineHeader, "v") == 0){
+
+			glm::vec3 vertex;
+			fscanf(file, "%f %f %f\n", &vertex.x, &vertex.y, &vertex.z);
+			//std::cout << "vertices : (x,y,z) = (" << vertex.x << ", " << vertex.y << ", " << vertex.z << ") \n";
+			temp_vertices.push_back(vertex);
 		}
-		glEnd();
-	}
-	glPopMatrix();
-	glEndList();
-	fclose(fp);
+		else if (strcmp(lineHeader, "vt") == 0){
+			glm::vec2 uv;
+			fscanf(file, "%f %f\n", &uv.x, &uv.y);
+			temp_uvs.push_back(uv);
+		}
+		else if (strcmp(lineHeader, "vn") == 0){
+			glm::vec3 normal;
+			fscanf(file, "%f %f %f\n", &normal.x, &normal.y, &normal.z);
+			//std::cout << "normal : (x,y,z) = (" << normal.x << ", " << normal.y << ", " << normal.z << ") \n";
+			temp_normals.push_back(normal);
 
-	
+
+		}
+		else if (strcmp(lineHeader, "f") == 0){
+			std::string vertex1, vertex2, vertex3;
+			unsigned int vertexIndex[3], uvIndex[3], normalIndex[3];
+			int matches = fscanf(file, "%d %d %d\n", &vertexIndex[0], &vertexIndex[1], &vertexIndex[2]);
+			if (matches != 3){
+				printf("File can't be read by our simple parser : ( Try exporting with other options\n");
+			}
+			vertexIndices.push_back(vertexIndex[0]);
+			vertexIndices.push_back(vertexIndex[1]);
+			vertexIndices.push_back(vertexIndex[2]);
+		}
+
+		
+
+
+		//i += 3;
+		//std::cout << "current index is " << i << "/n";
+	}
+
+	for (unsigned int i = 0; i < vertexIndices.size(); i++){
+
+		unsigned int vertexIndex = vertexIndices[i];
+
+		printf("vertex Index = %d \n", vertexIndex-1);
+
+		glm::vec3 vertex = temp_vertices[vertexIndex - 1];
+
+		out_vertices.push_back(vertex);
+	}
+
+
+	for (unsigned int i = 0; i < normalIndices.size(); i++){
+		unsigned int normalIndex = normalIndices[i];
+		glm::vec3 normal = temp_normals[normalIndex - 1];
+		out_normals.push_back(normal);
+	}
+
+	return true;
 
 
 }
 
+
+
 void drawBig() {
 
-	int read;
-	GLfloat x, y, z;
-	char ch;
 
 
-
-	//glTranslatef(0, -40, -105);
+	glBegin(GL_LINES);
 	glColor3f(1.0, 0, 0);
-
-	std::cout << ("Here : 1 \n");
-	std::cout << "Vertices" << vertices[0] << "\n";
-	std::cout << "Indices" << faces[0] << "\n";
-
-	glPointSize(3.0);
-	glBegin(GL_POINTS);
-
-	for (int a = 0; a < 18; a+=3) {
-		glVertex3d(vertices[a], vertices[a + 1], vertices[a + 2]);
+	for (int i = 0; i < vertices.size(); i++) {
+		glVertex3f(vertices[i].x, vertices[i].y, vertices[i].z);
+		printf("Vertex[%d] = (%f,%f,%f) \n", i, vertices[i].x, vertices[i].y, vertices[i].z);
 	}
+
+
 	glEnd();
 
-	/*glEnableClientState(GL_NORMAL_ARRAY);
-	glEnableClientState(GL_COLOR_ARRAY);
-	glEnableClientState(GL_VERTEX_ARRAY);
-	//glNormalPointer(GL_FLOAT, 0, normals);
-	//glColorPointer(3, GL_FLOAT, 0, colors2);
-	glVertexPointer(3, GL_FLOAT, 0, vertices);
-
-	glPushMatrix();
-	glTranslatef(-2, -2, 0);                // move to bottom-left corner
-
-	glDrawElements(GL_TRIANGLES, 27, GL_UNSIGNED_BYTE, faces);
-
-	glPopMatrix();
-
-	glDisableClientState(GL_VERTEX_ARRAY);  // disable vertex arrays
-	glDisableClientState(GL_COLOR_ARRAY);
-	glDisableClientState(GL_NORMAL_ARRAY);*/
-
-
-
-
-
-
-
-
-
-
-
-	//glRotatef(bigtrot, 0, 1, 0);
-	//glCallList(big);
-
-	//bigtrot = bigtrot + 0.6;
-	//if (bigtrot > 360) bigtrot = bigtrot - 360;
+	
 }
 
 void init(void) {
@@ -175,8 +183,9 @@ void init(void) {
 	glShadeModel(GL_FLAT);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	//glOrtho(-100, 100, -100, 100,0,100);
-	glOrtho(-10, 10, -10,10, 0,10);
+	//glOrtho(-20, 20, -20, 20,0,20);
+	glOrtho(-10, 10, -10, 10, 0, 10);
+	//glOrtho(-2, 2, -2, 2, 0, 1);
 	glMatrixMode(GL_MODELVIEW);
 
 
@@ -205,7 +214,7 @@ int main(int argc, char* argv[]){
 
 	// Create window
 	glutInitWindowPosition(0, 0);
-	glutInitWindowSize(900, 700);
+	glutInitWindowSize(700, 700);
 	glutCreateWindow("HW#2");
 
 	//  Enable Z-buffer depth test
@@ -213,9 +222,24 @@ int main(int argc, char* argv[]){
 
 	init();
 	// Callback functions
+	//glutDisplayFunc(display);
+
+
+
+	bool res = loadOBJ("lamp.obj", vertices, uvs, normals);
+
+	/*for (int i = 0; i < vertices.size(); i++) {
+		printf("Vertex[%d] = (%f,%f,%f) \n", i, vertices[i].x, vertices[i].y, vertices[i].z);
+	}
+	for (int i = 0; i < normals.size(); i++) {
+		printf("normals[%d] = (%f,%f,%f) \n", i, normals[0].x, normals[0].y, normals[0].z);
+	}*/
+	//glBufferData(GL_ARRAY_BUFFER, vertices.size()* sizeof(glm::vec3), &vertices.front(), GL_STATIC_DRAW);
 	glutDisplayFunc(display);
-	loadObj("before.obj");
-	
+
+
+	//loadObj("before.obj");
+
 	//  Pass control to GLUT for events
 	glutMainLoop();
 
